bdae81425ff63a28f2c178ecd6353d8c238feffe
Andrew Crist <andrew.l.crist@gmail.com>
2017-03-29 09:43:21 -0500

    Coming soon

--- src/components/App.js
+++ src/components/App.js
@@ -2,39 +2,19 @@ import React, { Component } from 'react';
 import { SideNav } from './SideNav'
 import Header from './Header'
 import Projects from './Projects'
+import {ComingSoon} from './ComingSoon'
 import '../styles/App.css';
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
-    this.state = {
-      animations: false,
-      menu: false
-    }
-  }
-
-  animateOut() {
-    this.setState({ animations: !this.state.animations, menu: !this.state.menu })
-  }
-
-  animateIn() {
-    this.setState({ animations: !this.state.animations, menu: !this.state.menu })
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <button onClick={() => this.animateOut()}>Menu</button>
-        <Projects />
-        <SideNav
-          animateIn={this.animateIn.bind(this)}
-          animations={this.state.animations}
-          menu={this.state.menu}
-        />
+        <ComingSoon />
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ComingSoon = () => {
+    return (
+      <div className="coming-soon-container">
+        <p className="coming-soon">COMING SOON</p>
+      </div>
+    );
+}
