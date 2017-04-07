d7df40d5c92d99e0bc821c5a1330458ce55475ef
Andrew Crist <andrew.l.crist@gmail.com>
2017-04-07 06:44:40 -0500

    WIP email me component

---
+++ src/components/Email.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Email = () => {
+  return (
+      <div className="email-container">
+        <form>
+          <input></input>
+          <input></input>
+          <input></input>
+        </form>
+      </div>
+    )
+}
