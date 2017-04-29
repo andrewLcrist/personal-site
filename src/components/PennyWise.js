3d7655322d04c7a5654e63f649b5d72f9a41742b
Andrew Crist <andrew.l.crist@gmail.com>
2017-04-28 19:50:46 -0500

    WIP mock out project components

---
+++ src/components/PennyWise.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const PennyWise = () => {
+  return (
+      <div className="project-container">
+        <h1>PennyWise</h1>
+        <a href="https://www.google.com" target="_blank"><h3>View on GitHub</h3></a>
+        <a href="https://www.google.com" target="_blank"><h3>View Production</h3></a>
+        <h2>Project Overview</h2>
+        <p>Application that allows users to keep track of their movie collection and the format in which they own each individual movie.<br/>Movie information was acquired using The Movie Database API.</p>
+      </div>
+    )
+}
